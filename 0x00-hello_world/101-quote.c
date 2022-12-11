abd4db987f4e3346ccaa93afb27e4d71f36d0055
aqib-abdullahi <abdullahiaqib@gmail.com>
2022-12-11 13:16:15 -0800

    C program that writes to standard error solution.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -15,9 +15,11 @@
 int main(void)
 
 {
+	
+	int s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	write(STDOUT_FILENO, "and that piece of art is useful
-				\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful
+				\" - Dora Korpar, 2015-10-19\n", s);
 	return (1);
 
 }
