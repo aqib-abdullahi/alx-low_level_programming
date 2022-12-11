839b6acfba693efe325313333a2dbb5da1758e1d
aqib-abdullahi <abdullahiaqib@gmail.com>
2022-12-11 12:29:04 -0800

    program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: Printing to standard error
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	putchar('\n');
+	return (1);
+
+}
