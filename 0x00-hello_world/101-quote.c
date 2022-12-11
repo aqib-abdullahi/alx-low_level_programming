b7cfd6535eab69a104eaac21c334d9cfc476624d
aqib-abdullahi <abdullahiaqib@gmail.com>
2022-12-11 13:34:52 -0800

    C program that writes to standard error solution.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
-#include <stdlib.h>
-
 #include <unistd.h>
 
 /**
  *
- * main - Entry point
+ * main - Prints out the last part of a quote in the standard error.
  *
- * Return: Always 1 (Success)
+ * Return: 1 if success.
+ *
  */
 
 int main(void)
 
 {
-	int s;
-	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	write(2, "and that piece of art is useful
-				\" - Dora Korpar, 2015-10-19\n", s);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+
 	return (1);
 
 }
+
+
