6f7110f9f64220fc54d018b73fefe2ea15d79343
aqib-abdullahi <abdullahiaqib@gmail.com>
2022-12-14 08:19:06 -0800

    a function that computes the absolute value of an integer.

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * int _abs - Absolute valuu of a number
- * @r :An input 
+ * _abs - Absolute valuu of a number
+ * @r :An input
  * Description: Computing the absolute value of an input
  * Return: Absolute value of an input
  */
