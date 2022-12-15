390f03b21d88e81b07cfbb60fbc0c011d11a14f1
aqib-abdullahi <abdullahiaqib@gmail.com>
2022-12-15 15:20:33 -0800

    FizzBuzz test correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,3 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
 
 /**
@@ -12,20 +10,20 @@ int main(void)
 {
 	int x;
 
-	x = 0;
+	x = 1;
 
-	for (x = 0; x <= 100; x++)
+	for (x = 1; x <= 100; x++)
 	{
 		if (x % 3 == 0)
-			printf("Fizz");
+			printf(" Fizz");
 		else if (x % 5 == 0)
-			printf("Buzz");
+			printf(" Buzz");
 		else if ((x % 3 == 0) && (x % 5 == 0))
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 		else
-			printf("%i", x);
+			printf(" %i", x);
 		
 	}
-	putchar('\n');
+	printf("\n");
 
 }
