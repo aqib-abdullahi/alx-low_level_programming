ad00764314eb36f73734df95b3eb3ce5bfb689b6
aqib-abdullahi <abdullahiaqib@gmail.com>
2022-12-15 12:45:42 -0800

    function modification

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,8 +4,10 @@
 /**
  * print_most_numbers - a function tha prints numbers
  * from 0 to 9 except 2 and 4
+ *
  * Description: Printing 0 to 9 except 2 and 4
- * Return: 0 always
+ *
+ * Return: void
  */
 
 void print_most_numbers(void)
