599db3d9814d61d5fa84b7d01b6cd4392acdc470
aqib-abdullahi <abdullahiaqib@gmail.com>
2022-12-15 09:46:19 -0800

    Checking for digits 0 through 9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,8 +9,19 @@
 
 int _isdigit(int c)
 {
-	if (c > 47 && c < 58)
-		return (1);
 
-	return (0);
+	char i = '0';
+
+	int isdigit = 0;
+
+		for (; i <= '9'; i++)
+		{
+			if (i == c)
+			{
+				isdigit = 1;
+				break;
+			}
+		}
+		return (isdigit);
+
 }
