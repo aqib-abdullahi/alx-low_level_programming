9c819c6747b55ed2ec1dc2eda1ac1c20bc2b1a5a
aqib-abdullahi <abdullahiaqib@gmail.com>
2022-12-15 10:36:58 -0800

    Function for multiplying two ints

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * mul - Multiplying two integers
+ * @a - int type number
+ * @b - int type number
+ * Description: Function for multiplying two integers
+ * Return: No return
+ */
+
+int mul(int a, int b)
+{
+
+	int mult;
+	
+	mult = a * b;
+
+	printf("%i", mult);
+
+	return (0);
+
+
+}
