1334b6a187a0da1c813aadfd5c023630322808ad
aqib-abdullahi <abdullahiaqib@gmail.com>
2022-12-19 13:09:30 -0800

    Printing characters in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - Printing charactera in reverse
+ * @s: char type character
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] = '\0')
+	{
+		putchar(s[i]);
+		i--;
+	}
+	putchar('\n');
+}
