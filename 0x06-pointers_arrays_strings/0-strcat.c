fc546b6fa203f8c492a51694f5d449a646ccbb9c
aqib-abdullahi <abdullahiaqib@gmail.com>
2022-12-21 13:33:49 -0800

    strcat function modification

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * *_strcat - a function that concatenates two strings
  * @dest - Destination string
  * @src - source string
- * Return: Nothing
+ * Return: pointer to resulting string
  */
 
 char *_strcat(char *dest, char *src)
 {
 	int len;
 	int j;
+	char *re;
 
 	len = 0;
 	j = 0;
@@ -21,5 +23,7 @@ char *_strcat(char *dest, char *src)
 	for (j = 0; src[j] != '\0'; ++j, ++len)
 		dest[len] = src[j];
 	dest[len] = '\0';
+	*re = dest[100];
 
+	return (re);
 }
