2a0f0303d8c9461216fc3378c95a9453347d9779
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-01-03 00:38:47 -0800

    memcpy correction

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,9 +3,10 @@
 /**
  * _memcpy - a function that copies
  * n bytes from memory area src to memory area dest
- * @*dest: destination of copied bytes
- * @*src: source of bytes to be copied
+ * @dest: destination of copied bytes
+ * @src: source of bytes to be copied
  * @n: int type input
+ * Return: returns pointer to copy location
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
