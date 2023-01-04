3d0f7bd25a0fbb555efe0fce028d36a55fbae375
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-01-04 13:47:43 -0800

    string length file

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,13 +9,10 @@
 
 int _strlen_recursion(char *s)
 {
-	int i = 0;
+	int i = 1;
 
 	if (*s != '\0')
-	{
-		i += 1;
-		_strlen_recursion(s + 1);
-		return (i);
+		return (i + _strlen_recursion(s + i);
 	}
 	else
 		return (0);
