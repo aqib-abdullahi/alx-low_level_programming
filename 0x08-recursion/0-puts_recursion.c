7b7304ac5a622cabaef968eb7d240a27d020f6c7
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-01-04 12:58:17 -0800

    puts recursion file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - a function that prints
+ * a given string
+ * @s: input string
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		putchar(*s);
+		_puts_reursion(s + 1);
+	}
+	else
+		putchar('\n');
+}
