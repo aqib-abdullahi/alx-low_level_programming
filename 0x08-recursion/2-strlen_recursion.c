988fbe6fde64a98e3771bfb37121a3a60514f2a0
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-01-04 13:40:01 -0800

    string length file

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - a function that returns
+ * the length of a string
+ * @s: input string
+ * Return: Length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s != '\0')
+	{
+		i += 1;
+		_strlen_recursion(s + i);
+		return (i);
+	}
+}
