b987498d9ab53312fb614e6e92eaabe8bb535967
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-01-06 13:24:43 -0800

    whatsmyname file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its
+ * name followed by a new line
+ * Return: 0 success
+ */
+
+int main(int argc, char* argv[])
+{
+	printf("./%s", argv[0]);
+	return (0);
+}	
