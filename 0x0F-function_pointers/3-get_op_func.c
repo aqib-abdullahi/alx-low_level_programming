e123774d9868e27bb3c539de9c2908035d1a843a
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-01-18 14:36:13 -0800

    op function correction

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,8 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-
 /**
  * get_op_func - selects the correct function
  * to be used
@@ -13,16 +11,14 @@
 
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-	        {"+", op_add},
+	op_t ops[] = {{"+", op_add},
 		{"-", op_sub},
-	        {"*", op_mul},
-	        {"/", op_div},
-	        {"%", op_mod},
-	        {NULL, NULL}
-	    };
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
 	int i = 0;
-	
+
 	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
