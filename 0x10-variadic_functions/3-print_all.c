ff943d4600413ed028f3c2d2dda2affe988454c6
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-01-25 15:48:55 -0800

    correction task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,7 +7,7 @@
  * print_all - prints all string inputs
  * @format:list of types of arguments passed
  * to the function
- * @char:character seperating strings
+ * @format:character seperating strings
  * Return: Nothing
  */
 
@@ -46,7 +46,8 @@ void print_all(const char * const format, ...)
 				printf("(nil)");
 				break;
 		}
-		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||format[i] == 's') && format[(i + 1)] != '\0')
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' 
+		|| format[i] == 's') && format[(i + 1)] != '\0')
 			printf(", ");
 		i++;
 	}
