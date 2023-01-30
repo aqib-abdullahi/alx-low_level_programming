9f1248180946b39e3974fd4ed78a87246803d8e1
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-01-30 13:01:42 -0800

    print listint task 0 correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,7 +14,7 @@ size_t print_listint(const listint_t *h)
 	const listint_t *current_node = h;
 	size_t node_count = 0;
 
-	while (*current_node != NULL)
+	while (current_node != NULL)
 	{
 		printf("%d", current_node->n);
 		node_count += 1;
