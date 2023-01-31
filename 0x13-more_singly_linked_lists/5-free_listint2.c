b831416c7bf42cf2d19a88dd2ff1c2a39921c401
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-01-31 07:57:32 -0800

    free list task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,13 +13,14 @@ void free_listint2(listint_t **head)
 	listint_t *tmp;
 
 	if (head == NULL)
+	{
 		return;
+	}
 	while (*head)
 	{
 		tmp = (*head)->next;
 		free(*head);
 		*head = tmp;
 	}
-
 	*head = NULL
 }
