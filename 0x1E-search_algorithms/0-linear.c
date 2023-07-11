041616328493386bd8b64d36b9959e258f809ab6
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-07-11 03:06:53 -0700

    header file and Linear search algorithm file added

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+/* linear_search - searches for values in an array
+ * of integers
+ * @array: array to be searched
+ * @size: number of elements in an array
+ * @value: value to be searched for
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	for (; i < size; ++i)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+			break;
+	}
+
+	if (i == size)
+		return (-1);
+
+	return (i);
+}
