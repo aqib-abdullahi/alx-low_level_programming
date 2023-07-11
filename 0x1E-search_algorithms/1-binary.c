da4c1784d74cd0ebfc0f98bb03a0ef3811736199
aqib-abdullahi <abdullahiaqib@gmail.com>
2023-07-11 03:43:35 -0700

    Binary ssearch algorithm implemented and betty compliant

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,37 +1,47 @@
 #include "search_algos.h"
+/**
+ * binary_search - Searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
+ * @array: A pointer to the first element of the array to search in
+ * @size: The number of elements in array
+ * @value: The value to search for
+ *
+ * Return: The first index where value is located
+ */
 
 int binary_search(int *array, size_t size, int value)
 {
 
-    size_t i = 0;
-    size_t left = 0;
-    size_t right = size - 1;
-    size_t mid;
+	size_t i = 0;
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t mid;
 
-    if (array == NULL)
-        return -1;
+	if (array == NULL)
+	return (-1);
 
-    while (left <= right)
-    {
-        mid = (left + right) / 2;
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
 
-        printf("Searching in array: ");
-        for (i = left; i <= right; i++)
-        {
-            if (i != left)
-                printf(", ");
-            printf("%d", array[i]);
-        }
-        printf("\n");
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i != left)
+				printf(", ");
+			printf("%d", array[i]);
+		}
+		printf("\n");
 
-        if (array[mid] == value)
-            return mid;
+		if (array[mid] == value)
+			return (mid);
 
-        if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
 
-    return -1;
+	return (-1);
 }
